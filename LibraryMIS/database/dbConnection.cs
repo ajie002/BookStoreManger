a2e828cf1dc8e037372b3f495b61b72d98acb3de
hwman <hwman@DESKTOP-HWMAN>
2019-10-21 17:58:16 +0800

    添加项目文件。

---
+++ LibraryMIS/database/dbConnection.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace LibraryMIS.database
+{
+	/// <summary>
+	/// dbConnection 的摘要说明。
+	/// </summary>
+	public class dbConnection
+	{
+		public dbConnection()
+		{
+			//
+			// TODO: 在此处添加构造函数逻辑
+			//
+		}
+		public static string connection
+		{
+			get
+			{
+                //return "server=ZZTI-SOFTTECH\\SQL2005;database=libraryMIS;uid=sa;pwd=123";
+                return @"Data Source=.\sqlexpress;Initial Catalog=LibraryMIS;Integrated Security=True";
+            }
+		}
+	}
+}
