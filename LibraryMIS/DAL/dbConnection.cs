2866ec3f20ce5b5de2d7d5e9911e7cde57bbc989
江贤湘 <1940232208@qq.com>
2022-07-11 16:52:55 +0800

    修改数据库连接

--- LibraryMIS/DAL/dbConnection.cs
+++ LibraryMIS/DAL/dbConnection.cs
@@ -18,7 +18,7 @@ namespace LibraryMIS.database
 			get
 			{
                 //return "server=ZZTI-SOFTTECH\\SQL2005;database=libraryMIS;uid=sa;pwd=123";
-                return @"Data Source=.\sqlexpress;Initial Catalog=LibraryMIS;Integrated Security=True";
+                return @"Data Source=.;Initial Catalog=shiyan7;Integrated Security=True";
             }
 		}
 	}
